f273bbd72c95a89adf09cff028b05253fe631c5a
Mario <kuro060306@gmail.com>
2024-09-06 04:08:34 -0500

    Intento 1 - Mallqui Vega

---
+++ src/lab03/OperacionesBasicas.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab03;
+
+/**
+ *
+ * @author casa
+ */
+public class OperacionesBasicas {
+    public double sumar(double numero1, double numero2){
+        return numero1 + numero2;
+    }
+    
+    public double restar(double numero1, double numero2){
+        return numero1 - numero2;
+    }
+    
+    public double multiplicar(double numero1, double numero2){
+        return numero1 * numero2;
+    }
+    
+    public double dividir(double numero1, double numero2){
+        return numero1 / numero2;
+    }
+}
